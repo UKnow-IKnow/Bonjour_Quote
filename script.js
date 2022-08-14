927e96e2a8bd511849b7ad740dd5b5685bed73bc
UKnow-IKnow <sarojsahu014@gmail.com>
2022-08-14 11:47:03 +0530

    bug fix

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 fetch("https://api.quotable.io/random")
     .then(data => data.json())
     .then(quoteData =>{
-        const quoteText = quoteData.content.text;
+        const quoteText = quoteData.content;
         const quoteElement = document.getElementById('quoteElement');
 
         quoteElement.innerHTML=quoteText
