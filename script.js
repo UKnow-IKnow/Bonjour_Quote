9badabf6555ae54964b92b6b7859977740ca7619
UKnow-IKnow <sarojsahu014@gmail.com>
2022-08-15 14:21:15 +0530

    just format

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 fetch("https://api.quotable.io/random")
     .then(data => data.json())
-    .then(quoteData =>{
+    .then(quoteData => {
         const quoteText = quoteData.content;
         const quoteElement = document.getElementById('quoteElement');
 
-        quoteElement.innerHTML=quoteText
+        quoteElement.innerHTML = quoteText
     })
